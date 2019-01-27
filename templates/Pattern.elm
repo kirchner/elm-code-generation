16dd79b60e2bbb801d372cd13ff6e498ed882c35
Fabian Kirchner <kirchner@posteo.de>
2019-01-27 15:02:31 +0100

    remove encodeA and aDecoder from template module

--- templates/Pattern.elm
+++ templates/Pattern.elm
@@ -14,12 +14,6 @@ module Pattern exposing
 
 -}
 
-import Json.Decode as Decode exposing (Decoder)
-import Json.Decode.CodeGeneration as Decode
-import Json.Decode.Pipeline as Decode
-import Json.Encode as Encode exposing (Value)
-import Json.Encode.CodeGeneration as Encode
-
 
 {-| This type represents either an `object` or a reference to an `object`.
 
@@ -52,27 +46,3 @@ type Point
         , angle : Float
         , distance : Float
         }
-
-
-encodeA : (object -> Value) -> A object -> Value
-encodeA encodeObject aObject =
-    case aObject of
-        That id ->
-            Encode.withType "that"
-                [ ( "name", Encode.int id ) ]
-
-        This object ->
-            Encode.withType "this"
-                [ ( "object", encodeObject object ) ]
-
-
-aDecoder : Decoder object -> Decoder (A object)
-aDecoder objectDecoder =
-    Decode.oneOf
-        [ Decode.succeed That
-            |> Decode.required "id" Decode.int
-            |> Decode.ensureType "that"
-        , Decode.succeed This
-            |> Decode.required "object" objectDecoder
-            |> Decode.ensureType "this"
-        ]
